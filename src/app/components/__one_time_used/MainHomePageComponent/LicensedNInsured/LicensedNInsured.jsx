11ce00bf5af55c0889680329d42f26d7cc8933b7
Tawhidul3233 <104293821+Tawhidul3233@users.noreply.github.com>
2024-03-18 16:32:57 +0600

    footer and servides area and licensed

--- src/app/components/__one_time_used/MainHomePageComponent/LicensedNInsured/LicensedNInsured.jsx
+++ src/app/components/__one_time_used/MainHomePageComponent/LicensedNInsured/LicensedNInsured.jsx
@@ -1,7 +1,59 @@
 import React from "react";
+import unlocke from '../../../../../../public/assets/licensed/unlock.png'
+import penter from '../../../../../../public/assets/licensed/pentair.png'
+import techo from '../../../../../../public/assets/licensed/techo.png'
+import latham from '../../../../../../public/assets/licensed/latham.png'
+import permacon from '../../../../../../public/assets/licensed/permacon.png'
+import Image from "next/image";
+
+
 
 const LicensedNInsured = () => {
-  return <div>LicensedNInsured LicensedNInsured</div>;
+
+  const imgList = [
+    {
+      img: unlocke
+    },
+    {
+      img: penter
+    },
+    {
+      img: techo
+    },
+    {
+      img: latham
+    },
+    {
+      img: permacon
+    }
+  ]
+
+
+  return <div>
+    <div className=" custom-container">
+      <div className="mx-auto lg:pt-28 pt-12 lg:pb-12 pb-8 flex flex-col items-center justify-center">
+
+        <div className='flex items-center pb-2'>
+          {/* <Image src={servicesIcon} className='h-5 w-10 mr-2' /> */}
+          <p>SINCE ’94</p>
+        </div>
+
+        <div>
+          <h2 className="lg:text-[36px] text-[20px]">LICENSED & INSURED</h2>
+        </div>
+
+      </div>
+
+
+    </div>
+    <div className=" flex justify-between lg:pb-16 pb-8">
+      {
+        imgList.map((im, i) => <div key={im.img}>
+          <img src={im.img.src} alt="" />
+        </div>)
+      }
+    </div>
+  </div>;
 };
 
 export default LicensedNInsured;
