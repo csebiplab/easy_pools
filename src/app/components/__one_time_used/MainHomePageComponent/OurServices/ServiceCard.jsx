4fe5190fc58d3958d46e57c2b3f64f8d8b4181ec
Oliulla <razasm29@gmail.com>
2024-03-19 00:18:08 +0600

    :sparkles: Done Our Services Section

---
+++ src/app/components/__one_time_used/MainHomePageComponent/OurServices/ServiceCard.jsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import React from "react";
+import ServiceCardButton from "./ServiceCardButton";
+
+const ServiceCard = ({ serv }) => {
+  const { imgUrl, title, desc } = serv ?? {};
+
+  return (
+    <div className="service__card">
+      <div className="text-center">
+        <Image
+          src={imgUrl}
+          width={400}
+          height={117}
+          className="W-full h-[117px]"
+        />
+        <div className="px-[14px]">
+          <h5 className="py-[25px]">{title}</h5>
+          <p className="pb-[45px]">{desc}</p>
+          <div className="pb-6 flex justify-center">
+            <ServiceCardButton />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ServiceCard;
--- src/app/components/ui/HeadingIcon.jsx
+++ src/app/components/ui/HeadingIcon.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const HeadingIcon = ({text = "empty text"}) => {
   return (
     <div className="flex items-center pb-2">
-      <Image src="/assets/licensed/sectionicon.png" width={48} height={16} className="h-4 w-12 mr-2" />
+      <Image src="/assets/licensed/sectionicon.png" width={48} height={16} className="h-4 w-12 mr-2 text-dark text-base" />
       <p>{text}</p>
     </div>
   );
