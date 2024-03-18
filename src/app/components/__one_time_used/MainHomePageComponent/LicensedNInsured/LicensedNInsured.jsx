c1dac16d60472fa8183ec22c8454ab931c8c86fe
Oliulla <razasm29@gmail.com>
2024-03-18 21:06:46 +0600

    :hammer: Refactor and :fire: split code

--- src/app/components/__one_time_used/MainHomePageComponent/LicensedNInsured/LicensedNInsured.jsx
+++ src/app/components/__one_time_used/MainHomePageComponent/LicensedNInsured/LicensedNInsured.jsx
@@ -1,60 +1,56 @@
 import React from "react";
-import unlocke from '../../../../../../public/assets/licensed/unlock.png'
-import penter from '../../../../../../public/assets/licensed/pentair.png'
-import techo from '../../../../../../public/assets/licensed/techo.png'
-import latham from '../../../../../../public/assets/licensed/latham.png'
-import permacon from '../../../../../../public/assets/licensed/permacon.png'
-import sectionIcon from '../../../../../../public/assets/licensed/sectionicon.png'
-import Image from "next/image";
-
-
+import unlocke from "../../../../../../public/assets/licensed/unlock.png";
+import penter from "../../../../../../public/assets/licensed/pentair.png";
+import techo from "../../../../../../public/assets/licensed/techo.png";
+import latham from "../../../../../../public/assets/licensed/latham.png";
+import permacon from "../../../../../../public/assets/licensed/permacon.png";
+import HeadingIcon from "@/app/components/ui/HeadingIcon";
+import { headingIconText } from "@/app/utils/heading-text";
 
 const LicensedNInsured = () => {
-
   const imgList = [
     {
-      img: unlocke
+      img: unlocke,
     },
     {
-      img: penter
+      img: penter,
     },
     {
-      img: techo
+      img: techo,
     },
     {
-      img: latham
+      img: latham,
     },
     {
-      img: permacon
-    }
-  ]
+      img: permacon,
+    },
+  ];
 
-
-  return <div>
-    <div className=" custom-container">
-      <div className="mx-auto lg:pt-28 pt-12 lg:pb-12 pb-8 flex flex-col items-center justify-center">
-
-        <div className='flex items-center pb-2'>
+  return (
+    <div>
+      <div className=" custom-container">
+        <div className="mx-auto lg:pt-28 pt-12 lg:pb-12 pb-8 flex flex-col items-center justify-center">
+          {/* <div className='flex items-center pb-2'>
           <Image src={sectionIcon} className='h-5 w-10 mr-2' />
           <p>SINCE ’94</p>
-        </div>
+        </div> */}
 
-        <div>
-          <h2 className="lg:text-[36px] text-[20px]">LICENSED & INSURED</h2>
-        </div>
+          <HeadingIcon text={headingIconText.licensed__IconTxt} />
 
+          <div>
+            <h2 className="lg:text-[36px] text-[20px]">LICENSED & INSURED</h2>
+          </div>
+        </div>
       </div>
-
-
+      <div className=" grid lg:grid-cols-5 md:grid-cols-3 grid-cols-2 md:mx-5 mx-3 gap-5 lg:pb-16 pb-8">
+        {imgList.map((im, i) => (
+          <div key={im.img}>
+            <img src={im.img.src} alt="" />
+          </div>
+        ))}
+      </div>
     </div>
-    <div className=" grid lg:grid-cols-5 md:grid-cols-3 grid-cols-2 md:mx-5 mx-3 gap-5 lg:pb-16 pb-8">
-      {
-        imgList.map((im, i) => <div key={im.img}>
-          <img src={im.img.src} alt="" />
-        </div>)
-      }
-    </div>
-  </div>;
+  );
 };
 
 export default LicensedNInsured;
