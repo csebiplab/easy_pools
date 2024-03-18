bcba88a816f4d2207924075d9afaa540ffcd218d
Tawhidul3233 <104293821+Tawhidul3233@users.noreply.github.com>
2024-03-18 21:27:50 +0600

    change something

--- src/app/components/__one_time_used/MainHomePageComponent/OurServiceAreas/OurServiceAreas.jsx
+++ src/app/components/__one_time_used/MainHomePageComponent/OurServiceAreas/OurServiceAreas.jsx
@@ -1,37 +1,35 @@
-"use client"
+"use client";
+import HeadingIcon from "@/app/components/ui/HeadingIcon";
+import { headingIconText } from "@/app/utils/heading-text";
 import React, { useState } from "react";
 
-
 const OurServiceAreas = () => {
-
   const [activeDot, setActiveDot] = useState(1);
 
   const handleDotClick = (dotNumber) => {
     setActiveDot(dotNumber);
   };
 
-
-  return <div>
-
-
-    <div className="custom-container">
-
-      <div className="mx-auto lg:pt-28 pt-12 lg:pb-12 pb-8 flex flex-col items-center justify-center">
-
-        <div className='flex items-center pb-2'>
-          {/* <Image src={servicesIcon} className='h-5 w-10 mr-2' /> */}
+  return (
+    <div>
+      <div className="custom-container">
+        <div className="mx-auto lg:pt-28 pt-12 lg:pb-12 pb-8 flex flex-col items-center justify-center">
+          {/* <div className='flex items-center pb-2'>
+          <Image src={servicesIcon} className='h-5 w-10 mr-2' />
           <p>Our Service Areas</p>
-        </div>
+        </div> */}
+          <HeadingIcon text={headingIconText.map__IconTxt} />
 
-        <div>
-          <h2 className="lg:text-[36px] text-[20px]">Serving the Greater Toronto Horseshoe Area for all your pool's needs.</h2>
+          <div>
+          <h2 className="lg:text-[36px] text-[20px]">
+            Serving the Greater Toronto Horseshoe Area for all your pool's
+            needs.
+          </h2>
         </div>
-
       </div>
 
       {/* //   ======================= map Card ==================// */}
       <div className=" grid md:grid-cols-3 grid-cols-2 lg:gap-18 gap-10">
-
         <div className="col-span-1 relative">
           <div className="shadow-md p-0 rounded-sm relative">
             <iframe
@@ -43,9 +41,13 @@ const OurServiceAreas = () => {
               referrerPolicy="no-referrer-when-downgrade"
             ></iframe>
             <div className="absolute inset-0 bg-black bg-opacity-70 flex items-center justify-center opacity-0 transition-opacity duration-300 hover:opacity-100">
-              <button className="bg-white py-2 px-4 rounded-lg shadow-md text-gray-800 hover:bg-gray-200 transition duration-300">Visit Location</button>
+              <button className="bg-white py-2 px-4 rounded-lg shadow-md text-gray-800 hover:bg-gray-200 transition duration-300">
+                Visit Location
+              </button>
             </div>
-            <h5 className="text-base py-4 px-10 bg-[#188B89] text-center lg:text-[18px] absolute bottom-0 left-0 right-0 text-white">Queens</h5>
+            <h5 className="text-base py-4 px-10 bg-[#188B89] text-center lg:text-[18px] absolute bottom-0 left-0 right-0">
+              Queens
+            </h5>
           </div>
         </div>
         <div className="col-span-1 relative">
@@ -59,9 +61,13 @@ const OurServiceAreas = () => {
               referrerPolicy="no-referrer-when-downgrade"
             ></iframe>
             <div className="absolute inset-0 bg-black bg-opacity-70 flex items-center justify-center opacity-0 transition-opacity duration-300 hover:opacity-100">
-              <button className="bg-white py-2 px-4 rounded-lg shadow-md text-gray-800 hover:bg-gray-200 transition duration-300">Visit Location</button>
+              <button className="bg-white py-2 px-4 rounded-lg shadow-md text-gray-800 hover:bg-gray-200 transition duration-300">
+                Visit Location
+              </button>
             </div>
-            <h5 className="text-base py-4 px-10 bg-[#188B89] text-center lg:text-[18px] absolute bottom-0 left-0 right-0 text-white">Queens</h5>
+            <h5 className="text-base py-4 px-10 bg-[#188B89] text-center lg:text-[18px] absolute bottom-0 left-0 right-0">
+              Queens
+            </h5>
           </div>
         </div>
         <div className="col-span-1 relative">
@@ -75,12 +81,15 @@ const OurServiceAreas = () => {
               referrerPolicy="no-referrer-when-downgrade"
             ></iframe>
             <div className="absolute inset-0 bg-black bg-opacity-70 flex items-center justify-center opacity-0 transition-opacity duration-300 hover:opacity-100">
-              <button className="bg-white py-2 px-4 rounded-lg shadow-md text-gray-800 hover:bg-gray-200 transition duration-300">Visit Location</button>
+              <button className="bg-white py-2 px-4 rounded-lg shadow-md text-gray-800 hover:bg-gray-200 transition duration-300">
+                Visit Location
+              </button>
             </div>
-            <h5 className="text-base py-4 px-10 bg-[#188B89] text-center lg:text-[18px] absolute bottom-0 left-0 right-0 text-white">Queens</h5>
+            <h5 className="text-base py-4 px-10 bg-[#188B89] text-center lg:text-[18px] absolute bottom-0 left-0 right-0">
+              Queens
+            </h5>
           </div>
         </div>
-
       </div>
 
       {/* dot button */}
@@ -95,8 +104,8 @@ const OurServiceAreas = () => {
         ))}
       </div>
     </div>
-
-  </div>
+    </ div>
+  );
 };
 
 export default OurServiceAreas;
