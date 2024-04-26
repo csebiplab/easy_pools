2b95292fbef72045425469f74e2fbce5cda96a62
Oliulla <razasm29@gmail.com>
2024-04-26 16:12:49 +0600

    :sparkles: Added dynamic metadata functionalities & google verification in home page

--- src/app/(pages)/page.js
+++ src/app/(pages)/page.js
@@ -1,48 +1,59 @@
 import MainHomePageComponent from "@/components/__one_time_used/MainHomePageComponent/MainHomePageComponent";
+import { pageName } from "@/constants/dashboard/pageName.constants";
 
 
 // Dynamic Metadata
-// export async function generateMetadata() {
-//   try {
-//     const metaDataResponse = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/home`);
-//     const metaData = await metaDataResponse.json();
+export async function generateMetadata() {
+  try {
+    const metaDataResponse = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/metaDatas?pageName=${pageName.home}`);
+    const metaData = await metaDataResponse.json();
+
+    const { data } = metaData ?? {};
+
+    const { title, description, keywords } = data ?? {}
 
 
 
-//     // Fetch Google site verification URL
-//     // const googleVerificationResponse = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/verificationUrl`);
-//     // const googleVerification = await googleVerificationResponse.json();
+    // Fetch Google site verification URL
+    const googleVerificationResponse = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/verificationUrl`);
+    const googleVerification = await googleVerificationResponse.json();
 
-//     // Extract Google console key from the meta tag content
-//     // const googleConsoleKey = extractGoogleConsoleKey(googleVerification);
+    // console.log("googleVerificationResponse", googleVerificationResponse)
+    // console.log("googleVerification", googleVerification)
 
-//     return {
-//       title: metaData?.homeRouteAllMetaData[0]?.title,
-//       description: metaData?.homeRouteAllMetaData[0]?.description,
-//       keywords: metaData?.homeRouteAllMetaData[0]?.keywords,
-//       // verification: {
-//       //   google: googleConsoleKey,
-//       // }
-//     };
-//   } catch (error) {
-//     return {
-//       title: "Easy pools | Home",
-//       keywords: "Home, Easy pools",
-//       description: "Easy pools"
-//     };
-//   }
-// }
+    // Extract Google console key from the meta tag content
+    const googleConsoleKey = extractGoogleConsoleKey(googleVerification);
 
-// function extractGoogleConsoleKey(googleVerification) {
-//   try {
-//     const { verificationUrl } = googleVerification ?? {};
-//     const metaTagContent = verificationUrl[0]?.title;
-//     const consoleKey = metaTagContent?.split(" ")[2]?.split("=")[1]?.slice(1, -1);
-//     return consoleKey;
-//   } catch (error) {
-//     console.error('Error extracting Google console key:', error);
-//   }
-// }
+    return {
+      title: title,
+      description: description,
+      keywords: keywords,
+      verification: {
+        google: googleConsoleKey,
+      }
+    }
+
+
+  } catch (error) {
+    console.log("object err:", error)
+    return {
+      title: "Easy pools | Home",
+      keywords: "Home, Easy pools",
+      description: "Easy pools"
+    };
+  }
+}
+
+function extractGoogleConsoleKey(googleVerification) {
+  try {
+    const { verificationUrl } = googleVerification ?? {};
+    const metaTagContent = verificationUrl[0]?.title;
+    const consoleKey = metaTagContent?.split(" ")[2]?.split("=")[1]?.slice(1, -1);
+    return consoleKey;
+  } catch (error) {
+    console.error('Error extracting Google console key:', error);
+  }
+}
 
 
 
