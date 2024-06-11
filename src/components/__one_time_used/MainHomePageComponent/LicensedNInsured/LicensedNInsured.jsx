08161d2e936f39218e64e3812023cf959d3387ee
Oliulla <razasm29@gmail.com>
2024-06-12 00:46:08 +0600

    done licensed

--- src/components/__one_time_used/MainHomePageComponent/LicensedNInsured/LicensedNInsured.jsx
+++ src/components/__one_time_used/MainHomePageComponent/LicensedNInsured/LicensedNInsured.jsx
@@ -1,9 +1,4 @@
 import React from "react";
-// import unlocke from "../../../../../../public/assets/licensed/unlock.png";
-// import penter from "../../../../../../public/assets/licensed/pentair.png";
-// import techo from "../../../../../../public/assets/licensed/techo.png";
-// import latham from "../../../../../../public/assets/licensed/latham.png";
-// import permacon from "../../../../../../public/assets/licensed/permacon.png";
 import { headingIconText } from "@/utils/heading-text";
 import Image from "next/image";
 import HeadingIcon from "@/components/ui/HeadingIcon";
@@ -31,11 +26,6 @@ const LicensedNInsured = () => {
     <div className=" container ">
       <div>
         <div className="mx-auto lg:pt-28 pt-12 lg:pb-12 pb-8 flex flex-col items-center justify-center">
-          {/* <div className='flex items-center pb-2'>
-          <Image src={sectionIcon} className='h-5 w-10 mr-2' />
-          <p>SINCE ’94</p>
-        </div> */}
-
           <HeadingIcon text={headingIconText.licensed__IconTxt} />
 
           <div>
@@ -43,7 +33,7 @@ const LicensedNInsured = () => {
           </div>
         </div>
       </div>
-      <div className=" flex justify-between md:mx-auto xs:mx-[30px] gap-5 lg:pb-16 pb-8">
+      <div className="flex justify-between items-center md:mx-auto xs:mx-[30px] gap-5 lg:pb-16 pb-8">
         {imgList.map((im, idx) => (
           <div key={idx}>
             <Image src={im.img} width={200} height={35} alt="licensed" />
