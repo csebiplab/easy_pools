ba5508678dcd25e179ab8cfb607cc68efbf9c4f0
Kongkon Jowarder <kongkon4545@gmail.com>
2024-07-02 20:06:41 +0600

    benifits, meanings and faq section problem solve

--- src/components/__one_time_used/FiberglassPoolInstallationInToronto/QA/QA.jsx
+++ src/components/__one_time_used/FiberglassPoolInstallationInToronto/QA/QA.jsx
@@ -7,73 +7,73 @@ import { headingIconText } from "@/utils/heading-text";
 const QAPoolInstallationQuestionsNAns = [
   {
     id: 1,
-    question: "What type of pool are you interested in?",
+    question: "What sets Easy Pools apart from other fiberglass pool installers?",
     answer:
-      "Options might include an inground pool, above-ground pool, fiberglass pool, vinyl-lined pool, or concrete pool.",
+      "Discover our commitment to quality, personalized service, and a seamless installation process that ensures your satisfaction.",
   },
   {
     id: 2,
-    question: "What size of the pool are you considering?",
+    question: "How long does it take to install a fiberglass pool with Easy Pools?",
     answer:
-      "Factors to consider include available space, intended use, and budget.",
+      "Our efficient installation process typically takes 7-10 days, ensuring you can enjoy your new pool in no time.",
   },
   {
     id: 3,
-    question: "What is your budget for the pool installation?",
+    question: "Are Easy Pools' fiberglass pools customizable?",
     answer:
-      "This will help determine what options are feasible and allow for appropriate planning.",
+      "Absolutely! We offer a range of designs, sizes, and features to suit your preferences, creating a tailor-made pool for your backyard.",
   },
   {
     id: 4,
-    question: "What permits are required for pool installation in your area?",
+    question: "What maintenance is required for Easy Pools' fiberglass pools?",
     answer:
-      "Local regulations may dictate the need for permits or inspections before, during, or after installation.",
+      "Our low-maintenance pools require minimal upkeep, and we provide clear guidelines to ensure your pool stays in pristine condition year-round.",
   },
   {
     id: 5,
-    question: "What is the timeline for the installation process?",
+    question: "Is financing available for Easy Pools' fiberglass pool installations?",
     answer:
-      "Understanding how long the installation will take can help with planning and coordination.",
+      "Yes, we offer flexible financing options to make turning your backyard into a personal oasis even more accessible.",
   },
   {
     id: 6,
-    question: "What maintenance will be required for the pool?",
+    question: "Can Easy Pools assist with landscaping around the fiberglass pool?",
     answer:
-      "Knowing the ongoing maintenance needs can help with budgeting and planning for the future.",
+      "We offer landscaping services to complement your new pool, creating a cohesive and visually appealing outdoor space.",
   },
   {
     id: 7,
     question:
-      "Are there any safety features you want to include with the pool?",
+      "What safety features are integrated into Easy Pools' fiberglass pools?",
     answer:
-      "This might include fencing, alarms, or pool covers to enhance safety.",
+      "Safety is a priority. Our pools are equipped with top-tier safety features, including non-slip surfaces and compliant fencing options.",
   },
   {
     id: 8,
     question:
-      "What landscaping or additional features do you want around the pool?",
+      "How does the installation process with Easy Pools impact my property?",
     answer:
-      "Consideration of features like decking, landscaping, lighting, or seating areas can enhance the overall pool area.",
+      "Our experienced team ensures a minimally invasive installation, respecting your property and leaving it in excellent condition",
   },
   {
     id: 9,
-    question: "Can the pool accommodate any future additions or modifications?",
+    question: "Are Easy Pools environmentally conscious in their fiberglass pool installations?",
     answer:
-      "Thinking about future needs or desires, such as adding a hot tub or upgrading equipment, can inform decisions during the installation process.",
+      "Yes, we prioritize eco-friendly practices, utilizing sustainable materials and energy-efficient technology in our pool installations.",
   },
 ];
 
 const QA = () => {
   return (
     <div className="bg__fiberglassPage common__padding__top ">
-      <section className="container">
-        <div className="mx-auto xs:px-[35px]">
+      <section className="full__section_l_p">
+        <div className="mx-auto">
           <div className="mt-8 grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-12">
-            <div className="order-2 flex justify-center items-center ">
+            <div className="order-2 flex justify-end items-center pr-3 md:pr-0">
               <Image
                 alt="faq"
                 src="/assets/fiberglass/qa.png"
-                width={580}
+                width={893}
                 height={810}
                 className="h-dvh"
               />
@@ -81,17 +81,17 @@ const QA = () => {
 
             <div>
               <div>
-                <div className="order-1 flex flex-col  ">
+                <div className="order-1 flex flex-col">
                   <HeadingIcon text={headingIconText.qnda__inFiberglassText} />
 
                   <div>
-                    <h3 className="text-lg md:text-xl lg:text-2xl xl:text-[26px] 2xl:text-[28px] 3xl:text-3xl 4xl:text-[33px] 5xl:text-4xl font-normal text-center md:text-left text-black pt-[8px] md:pt-[16px] pb-[16px] md:pb-[36px]">
+                    <h3 className="text-lg md:text-xl lg:text-2xl xl:text-[26px] 2xl:text-[28px] 3xl:text-3xl 4xl:text-[33px] 5xl:text-4xl font-normal text-left text-black pt-[8px] md:pt-[16px] pb-[16px] md:pb-[36px]">
                       Questions & Answers for Pool Installation
                     </h3>
                   </div>
                 </div>
               </div>
-              <div className="">
+              <div className="pr-3 md:pr-0">
                 {QAPoolInstallationQuestionsNAns.map((ques, idx) => (
                   <div key={idx}>
                     <details className="group [&_summary::-webkit-details-marker]:hidden">
