e8b7cf6ee7e40889dd0ab34453895c23621820ad
Oliulla <razasm29@gmail.com>
2024-07-12 01:13:52 +0600

    fixed text overflow

--- src/components/__one_time_used/FiberglassPoolInstallationInToronto/FiberglassPoolBanner/FiberglassPoolBanner.jsx
+++ src/components/__one_time_used/FiberglassPoolInstallationInToronto/FiberglassPoolBanner/FiberglassPoolBanner.jsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Button } from "@material-tailwind/react";
-import Link from "next/link";
 import { FaChevronRight } from "react-icons/fa6";
 import "./FiberglassPoolBanner.css";
 
@@ -10,15 +9,15 @@ const FiberglassPoolBanner = () => {
     <>
       <div className="hero-section container">
         <div className="md:flex items-center relative h-full">
-          <div className="flex items-center w-full h-full  md:w-[58%] pl-3 md:pl-5 lg:pl-10 xl:pl-20 2xl:pl-28 3xl:pl-40 4xl:pl-52 5xl:pl-[285px]">
+          <div className="flex items-center w-full h-full  md:w-[52%] pl-3 md:pl-5 lg:pl-10 xl:pl-20 2xl:pl-28 3xl:pl-40 4xl:pl-52 5xl:pl-[285px]">
             <div className=" py-[300px] md:py-0 px-4 md:pl-0">
               <h1 className="leading-normal text-[34px] md:text-[36px] xl:text-[38px] 2xl:text-[42px] 3xl:text-[46px] 4xl:text-[50px] 5xl:text-[55px] font-bold text-left">
                 Fiberglass Pool <br /> Installation in Toronto
               </h1>
               <p className="text-base lg:text-lg font-normal mb-5 leading-6 mt-4 text-black">
                 Easy Pools is the best fiberglass pool installation company in
-                Toronto <br /> for ensuring a hassle-free installation process
-                for modern living.
+                Toronto for ensuring a hassle-free installation process for
+                modern living.
               </p>
               <div className="mb-4 md:mb-0">
                 <a href="tel:+1(647) 449 9512" className="inline-block">
