5ed57be154f80ff45e3154bb222159130e478d2f
Oliulla <razasm29@gmail.com>
2024-08-17 21:09:32 +0600

    done create part

--- src/app/(dashboard)/dashboard/allpages/create-all-pages-metadata/page.js
+++ src/app/(dashboard)/dashboard/allpages/create-all-pages-metadata/page.js
@@ -1,44 +1,18 @@
 "use client";
 
-import ShareComponent from "@/components/__shared/dashboard/ShareComponent";
-import { pageName } from "@/constants/dashboard/pageName.constants";
-import useFetchData from "@/hooks/useFetchData";
+import CreateAndUpdateFormForPageMeta from "@/components/__shared/dashboard/CreateAndUpdateFormForPageMeta";
 import { useState } from "react";
 
 
 const page = () => {
-    const editRoute = "dashboard/allpages/home/editVinyl";
-    const pagename = pageName?.vinylPool;
-    const baseAPIUrl = process.env.NEXT_PUBLIC_API_URL + `/api/metaDatas?pageName=${pagename}`
-
-    const [isCreateRUpdateFinished, setIsCreateRUpdateFinished] = useState(false);
     const [isUpdateCreateLoading, setIsUpdateCreateLoading] = useState(false);
-    const { data, loading, error } = useFetchData(baseAPIUrl, isCreateRUpdateFinished)
-
-
-
-    if (loading) {
-        return <p>Loading...</p>;
-    }
-
-    if (error) {
-        return <p>Error: {error.message}</p>;
-    }
-
-
-    const metaData = data?.data;
-
 
 
     return (
         <>
-            <ShareComponent
-                editPath={editRoute}
-                metaData={metaData}
-                pagename={pagename}
+            <CreateAndUpdateFormForPageMeta
                 isUpdateCreateLoading={isUpdateCreateLoading}
                 setIsUpdateCreateLoading={setIsUpdateCreateLoading}
-                setIsCreateRUpdateFinished={setIsCreateRUpdateFinished}
             />
         </>
     );
