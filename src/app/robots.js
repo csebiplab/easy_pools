3a490a9b6f761d6effefa4502261b2c76763676f
Oliulla <razasm29@gmail.com>
2024-08-19 23:05:23 +0600

    done added robots.txt page

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,31 +1,33 @@
-import connectMongoDB from "@/libs/db";
-import RobotTxt from "@/models/robot.model";
-
 export default async function robots() {
-    await connectMongoDB();
-    const data = await RobotTxt.find({});
-
-    if (data?.length < 1) {
-        return {
-            rules: [
-                {
-                    userAgent: '*',
-                    allow: ['/'],
-                    disallow: ['/private/'],
-                },
-            ],
-            sitemap: `${process.env.NEXT_PUBLIC_API_URL}/sitemap.xml`,
-        };
-    }
-
-    const rules = data.map(item => ({
-        userAgent: item.user_agent,
-        allow: item.allow,
-        disallow: item.disallow
-    }));
-
-    return {
-        rules,
-        sitemap: `${process.env.NEXT_PUBLIC_API_URL}/sitemap.xml`,
+    const sitemapUrl = `${process.env.NEXT_PUBLIC_API_URL}/sitemap.xml`;
+    const robotStatic = {
+        rules: [
+            {
+                userAgent: '*',
+                allow: ['/public/'],
+                disallow: ['/private/'],
+            },
+        ],
+        sitemap: sitemapUrl,
     };
+
+    try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/robotTxt`, {
+            cache: "no-store",
+        });
+        const { data } = await response.json();
+
+        const rules = data?.map((singleData) => ({
+            userAgent: singleData?.user_agent || '*',
+            allow: singleData?.allow || ['/public/'],
+            disallow: singleData?.disallow || ['/private/'],
+        }));
+
+        return {
+            rules: rules?.length > 0 ? rules : robotStatic.rules,
+            sitemap: sitemapUrl,
+        };
+    } catch (error) {
+        return robotStatic;
+    }
 }
