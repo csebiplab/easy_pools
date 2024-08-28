302033052cb8e7bb7190b9185f624c67fcf18f99
Kongkon Jowarder <kongkon4545@gmail.com>
2024-08-28 16:51:32 +0600

    banner, footer image name change and trustUs section problem solve

--- src/components/Services/SwimingPoolInstallationInVaughan/SwimmingPoolBanner/SwimmingPoolBanner.jsx
+++ src/components/Services/SwimingPoolInstallationInVaughan/SwimmingPoolBanner/SwimmingPoolBanner.jsx
@@ -7,7 +7,7 @@ const SwimmingPoolBanner = () => {
     <>
       <div className="hero__section__vaughan custom-container">
         <div className="md:flex items-center relative">
-          <div className="w-full md:w-[58%] pl-3 md:pl-5 lg:pl-10 xl:pl-20 2xl:pl-28 3xl:pl-40 4xl:pl-52 5xl:pl-[285px]">
+          <div className="w-full md:w-[50%] pl-3 md:pl-5 lg:pl-10 xl:pl-20 2xl:pl-28 3xl:pl-40 4xl:pl-52 5xl:pl-[285px]">
             <div className="py-14 md:py-0 px-4 md:pl-0">
               <h1 className="leading-normal text-[36px] md:text-[40px] lg:text-[43px] xl:text-[46px] 2xl:text-[48px] 3xl:text-[52px] 4xl:text-[56px] 5xl:text-[60px] font-bold text-left text-black">
               Swimming Pool Installation in Vaughan
@@ -26,7 +26,7 @@ const SwimmingPoolBanner = () => {
             <Image
               width={858}
               height={832}
-              src="/assets/swimmingPool/hero-section.jpg"
+              src="/assets/swimmingPool/hero__section.jpg"
               alt="hero small image"
               className="hero-main-image mb-0"
             />
