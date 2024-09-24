79cea9da4b1af45fbf2e49a3bc6dbb96f3361c52
Kongkon Jowarder <kongkon4545@gmail.com>
2024-09-24 14:50:34 +0600

    banner and partnerNRatings section add

--- src/components/__one_time_used/MainHomePage/MainHomePage.jsx
+++ src/components/__one_time_used/MainHomePage/MainHomePage.jsx
@@ -1,55 +1,66 @@
-import React from 'react';
+import React from "react";
 import "./MainHomePage.css";
-import AboutUs from './AboutUs/AboutUs';
-import LeadingToronto from './LeadingToronto/LeadingToronto';
-import TrustedPartner from './TrustedPartner/TrustedPartner';
-import DesignCustomization from './DesignCustomization/DesignCustomization';
-import MaterialsEquipment from './MaterialsEquipment/MaterialsEquipment';
-import Specialists from './Specialists/Specialists';
-import HireFor from './HireFor/HireFor';
-import WorkingProcess from './WorkingProcess/WorkingProcess';
-import ConsiderationsLimitations from './ConsiderationsLimitations/ConsiderationsLimitations';
+import AboutUs from "./AboutUs/AboutUs";
+import LeadingToronto from "./LeadingToronto/LeadingToronto";
+import TrustedPartner from "./TrustedPartner/TrustedPartner";
+import DesignCustomization from "./DesignCustomization/DesignCustomization";
+import MaterialsEquipment from "./MaterialsEquipment/MaterialsEquipment";
+import Specialists from "./Specialists/Specialists";
+import HireFor from "./HireFor/HireFor";
+import WorkingProcess from "./WorkingProcess/WorkingProcess";
+import ConsiderationsLimitations from "./ConsiderationsLimitations/ConsiderationsLimitations";
+import PartnerNRatings from "../MainHomePageComponent/PartnerNRatings/PartnerNRatings";
+import Banner from "./Banner/Banner";
 
 const MainHomePage = () => {
-    return (
-        <div className='homePage__bg'>
-            <section>
-                <AboutUs/>
-            </section>
+  return (
+    <div className="homePage__bg custom-container">
 
-            <section>
-                <LeadingToronto/>
-            </section>
+      <section>
+        <Banner/>
+      </section>
 
-            <section>
-                <TrustedPartner/>
-            </section>
+      <section>
+        <PartnerNRatings classNames={{ bg: "homePage__bg" }} />
+      </section>
 
-            <section>
-                <DesignCustomization/>
-            </section>
+      <section>
+        <AboutUs />
+      </section>
 
-            <section>
-                <MaterialsEquipment/>
-            </section>
+      <section>
+        <LeadingToronto />
+      </section>
 
-            <section>
-                <Specialists/>
-            </section>
+      <section>
+        <TrustedPartner />
+      </section>
 
-            <section>
-                <WorkingProcess/>
-            </section>
+      <section>
+        <DesignCustomization />
+      </section>
 
-            <section>
-                <ConsiderationsLimitations/>
-            </section>
+      <section>
+        <MaterialsEquipment />
+      </section>
 
-            <section>
-                <HireFor/>
-            </section>
-        </div>
-    );
+      <section>
+        <Specialists />
+      </section>
+
+      <section>
+        <WorkingProcess />
+      </section>
+
+      <section>
+        <ConsiderationsLimitations />
+      </section>
+
+      <section>
+        <HireFor />
+      </section>
+    </div>
+  );
 };
 
 export default MainHomePage;
